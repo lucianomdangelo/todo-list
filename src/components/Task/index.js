98f5db53b5542d3fde4c88309a2f1049b350d240
lucianomdangelo <lucianomdangelo@gmail.com>
2022-06-15 10:16:13 -0300

    fix checkbox on reload

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -7,7 +7,7 @@ export default function Task({ id, task, complete }) {
 
   return (
     <div className='task' >
-        <input className="cb" type="checkbox"  onChange={toogleTask} />
+        <input className="cb" type="checkbox" defaultChecked={complete}  onChange={toogleTask} />
         <span className={ complete ? 'task-done' : 'task-undone' }>{ task }</span>
     </div>
   )
